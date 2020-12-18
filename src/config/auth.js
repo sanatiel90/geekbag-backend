1dcc9240d512bd92ca072b00636e4749aaea0fc6
sanatiel <sanatielbarros90@gmail.com>
2020-12-18 14:34:16 -0300

    iniciando

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'da3fd2ab3fac9ad0d204d8c211661857',
+    expiresIn: '7d'
+}
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import app from './app'
+
+app.listen(3333, () => console.log('Started at port 3333'))
+
+
+
+
+
+
+
+
+
+
+
+
